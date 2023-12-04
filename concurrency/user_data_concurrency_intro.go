57c38479ddd32168512f5dd85735eab5b7799dc9
Ashef Habib Tishad <ashefgo@gmail.com>
2023-12-05 04:13:39 +0600

    Tut/anthony gg (#2)
    * Before concurrency.
    time elapsed: 203.539417ms
    
    * Concurrently access user data example.
    * waitgroups
    * goroutines
    * channels
    
    * Added make command for golangci-lint run

---
+++ concurrency/user_data_concurrency_intro.go
@@ -0,0 +1,57 @@
+package concurrency
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func UserDataConcurrencyIntro() {
+	start := time.Now()
+	userID := 1
+
+	var wg sync.WaitGroup
+
+	// buffered channel is for accommodating the exact number of responses I am expecting.
+	respCh := make(chan string, 3)
+
+	wg.Add(3)
+
+	go fetchUserData(userID, respCh, &wg)
+	go fetchUserLikes(userID, respCh, &wg)
+	go fetchUserRecommendation(userID, respCh, &wg)
+
+	wg.Wait()
+
+	close(respCh)
+
+	for resp := range respCh {
+		fmt.Println(resp)
+	}
+
+	fmt.Println("time elapsed:", time.Since(start))
+}
+
+func fetchUserData(userID int, respCh chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(50 * time.Millisecond)
+
+	respCh <- fmt.Sprintf("fetched data for user_id: %d", userID)
+}
+
+func fetchUserLikes(userID int, respCh chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(50 * time.Millisecond)
+
+	respCh <- fmt.Sprintf("fetched likes for user_id: %d", userID)
+}
+
+func fetchUserRecommendation(userID int, respCh chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(100 * time.Millisecond)
+
+	respCh <- fmt.Sprintf("fetched recommendations for user_id: %d", userID)
+}
+
+// anthony gg
+// https://www.youtube.com/watch?v=P4tckkcyef0&list=PPSV
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/ashtishad/waytogo/concurrency"
+)
+
+func main() {
+	concurrency.UserDataConcurrencyIntro()
+}
