8c4130e0dc8ec006a0cadfdd5a0ba1a69bf3da91
ashtishad <ashefgo@gmail.com>
2023-12-20 10:18:48 +0600

    Leetcode 169: Majority element.
    Boyer-Moore Majority Voting Algorithm
    Time: O(n) and Space: O(1)
    
    https://www.topcoder.com/thrive/articles/boyer-moore-majority-vote-algorithm

--- leetcode/majorityelement169/boyer_moore_majority_voting_algo.go
+++ leetcode/majorityelement169/boyer_moore_majority_voting_algo.go
@@ -2,25 +2,58 @@ package majorityelement169
 
 // Boyer-Moore Majority Voting Algorithm
 // Time: O(n) and Space: O(1)
-//
-// For each element in the sequence:
-// - If the counter is zero, set the current candidate to the element, and initialize the counter to 1.
-// - If the counter is non-zero, increment it if the current candidate matches the element; otherwise, decrement the counter.
+// https://www.topcoder.com/thrive/articles/boyer-moore-majority-vote-algorithm
 
 func majorityElement(nums []int) int {
+	n := len(nums)
+
 	var candidate, count int
 
-	for _, num := range nums {
+	for _, x := range nums {
 		if count == 0 {
-			candidate, count = num, 1
+			candidate = x
+		}
+
+		if x == candidate {
+			count++
 		} else {
-			if candidate == num {
-				count++
-			} else {
-				count--
-			}
+			count--
 		}
 	}
 
-	return candidate
+	count = 0
+
+	for _, x := range nums {
+		if x == candidate {
+			count++
+		}
+	}
+
+	if count > n/2 {
+		return candidate
+	}
+
+	return -1
 }
+
+// Short code: For each element in the sequence:
+// - If the counter is zero, set the current candidate to the element, and initialize the counter to 1.
+// - If the counter is non-zero, increment it if the current candidate matches the element; otherwise, decrement the counter.
+
+// func majorityElement(nums []int) int {
+// 	var candidate, count int
+//
+// 	for _, num := range nums {
+// 		if count == 0 {
+// 			candidate, count = num, 1
+// 		} else {
+// 			if candidate == num {
+// 				count++
+// 			} else {
+// 				count--
+// 			}
+// 		}
+// 	}
+//
+// 	return candidate
+// }
